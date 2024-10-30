594cb8bacaab71bb47d856d39b7cd7417e02dea1
mohammadAbuAsaad <m7mad11114@gmail.com>
2024-10-30 05:50:19 +0200

    implement auth, appwirte

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import AuthForm from "@/components/AuthForm";
 
-const SignIn = ()=>{
+const SignIn = () => {
     return (
-        <div>sign-in</div>
+        <section className="flex-center size-full max-sm:px-6">
+            <AuthForm type="sign-in" />
+        </section>
     )
 }
 
